33e2dc2448afad03cc20f274581a6a317fc899c8
Agustin Vazquez <agustinvazquez453@gmail.com>
2021-02-15 11:37:34 -0300

    add html and scss support

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+console.log('Arrancada la aplicación compilando CSS y SaSS');
+
+/* CSS */
+import './styles/app.scss';
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,42 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.ts',
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          use: 'ts-loader',
-          exclude: /node_modules/,
-        },
-      ],
-    },
-    resolve: {
-      extensions: [ '.tsx', '.ts', '.js' ],
-    },
-    output: {
-      filename: 'app.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-  };
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/index.html',
+      hash: true
+    }),
+    new MiniCSSExtractPlugin({
+      filename: "./css/estilos.css",
+    })
+  ],
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCSSExtractPlugin.loader,
+          "css-loader",
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
